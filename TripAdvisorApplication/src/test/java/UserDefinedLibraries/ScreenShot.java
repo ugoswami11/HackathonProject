bf77877bb101b9976f7dc0c3cc3a1c8806bbc01b
RishavBasak <79359694+RishavBasak@users.noreply.github.com>
2021-02-22 17:07:00 +0530

    Update ScreenShot.java

--- TripAdvisorApplication/src/test/java/UserDefinedLibraries/ScreenShot.java
+++ TripAdvisorApplication/src/test/java/UserDefinedLibraries/ScreenShot.java
@@ -1,30 +1,29 @@
-package UserDefinedLibraries;
-
-/* Class  : Take Screenshot
- * Author : Aishwariya
- * Date   : 24-04-2020
- * ID     : 851297
- */
-
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
+
+//TAKING THE SCREEENSHOT
+
 public class ScreenShot {
-	public static String screenShotTC(WebDriver ldriver){
-		File sourceFile = ((TakesScreenshot) ldriver).getScreenshotAs(OutputType.FILE);
-		
-		File destFile = new File(
-				System.getProperty("user.dir") + "\\screenshot\\" + System.currentTimeMillis() + ".png");
-		try { 
-			//copy the screenshot to desired location using copyFile method
-			FileUtils.copyFile(sourceFile, destFile);
+	public static void ScreenshotTC(WebDriver driver) {
+		String filename;
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		filename = new SimpleDateFormat("yyyyMMddhhmmss'.jpeg'").format(new Date());
+			File destination = new File(
+					System.getProperty("user.dir")+"\\TestCaseScreenShots\\" + filename );
+		try {
+			FileUtils.copyFile(src, destination);
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
-		return destFile.getPath();
+		
 	}
+
 }
