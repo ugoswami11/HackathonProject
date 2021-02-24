1e3ee2e6e4c314b6886fe7e4bc49fdd9963958a6
shreyaGhosh2098 <79359784+shreyaGhosh2098@users.noreply.github.com>
2021-02-24 18:29:07 +0530

    Update HomePage.java

--- TripAdvisorApplication/src/test/java/testObjectRepository/HomePage.java
+++ TripAdvisorApplication/src/test/java/testObjectRepository/HomePage.java
@@ -1,5 +1,7 @@
 package testObjectRepository;
-
+/*
+This class locates all the elements present in TripAdvisor.in Home page and performs required action on them
+*/
 
 import java.util.List;
 
@@ -15,28 +17,28 @@ public class HomePage {
 	final WebDriver driver;
 	public static WebDriverWait wait;
 	
-	@FindBy(how =How.XPATH, using = "//span[contains(text(),'Holiday Homes')]")
+	@FindBy(how =How.XPATH, using = "//span[contains(text(),'Holiday Homes')]")			//locate "Holiday Homes" button
 	public WebElement holidayHomesBtn;
 	
 	@FindBy(how =How.XPATH, using = "//div[@class='i3bZ_gBa _2RTs3_Ee _3TPJs5_m _3awdcWrG']//input[@placeholder='Where to?']")
-	public WebElement whereTo;
+	public WebElement whereTo;									//locate "Where To" search box
 	
 	@FindBy(how = How.XPATH, using = "//div[@class='_27pk-lCQ']/a")
-	public List<WebElement> whereToDropDown; 
+	public List<WebElement> whereToDropDown; 							//locate "Where To" dropdown
 	
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
-		wait = new WebDriverWait(driver,30);
+		wait = new WebDriverWait(driver,30);							//waits untill the page get loaded
 	}
 	
 	public void holidayHomeButton() {
-		holidayHomesBtn.click();
+		holidayHomesBtn.click();								//clicking on "Holiday Homes" button
 	}
 	
 	public void setWhereTo(String destinationName) {
-		wait.until(ExpectedConditions.visibilityOf(whereTo));
+		wait.until(ExpectedConditions.visibilityOf(whereTo));					//waits until "Where To" search box is visible
 		
-		whereTo.sendKeys(destinationName);
+		whereTo.sendKeys(destinationName);							//sending value to the "Where To" search box
 		try {
 			Thread.sleep(4000);
 		} catch (InterruptedException e) {
@@ -47,7 +49,7 @@ public class HomePage {
 	
 	public void selectDestination() {
 		wait.until(ExpectedConditions.visibilityOfAllElements(whereToDropDown));
-		whereToDropDown.get(0).click();
+		whereToDropDown.get(0).click();								//clicking on the option present in the "Where To" dropdown
 
 	}
 	
